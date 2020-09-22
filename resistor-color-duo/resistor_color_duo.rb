cc4753c616602a1dc3094760907f2a6780ec8781
lxmrc <19227912+lxmrc@users.noreply.github.com>
2020-09-22 15:45:47 +1000

    Resistor Color Duo

--- resistor-color-duo/resistor_color_duo.rb
+++ resistor-color-duo/resistor_color_duo.rb
@@ -1,7 +1,7 @@
-class ResistorColorDuo
-  VALUES = %w(black brown red orange yellow green blue violet grey white)
+module ResistorColorDuo
+  VALUES = %w[black brown red orange yellow green blue violet grey white]
 
   def self.value(colors)
-    colors.first(2).map { |color| VALUES.index(color).to_s }.join.to_i
+    colors.first(2).map { |c| VALUES.index(c) }.join.to_i
   end
 end
