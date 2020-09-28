39b2d9e77adc1f42bf8e62cf0a878791b71cd784
lxmrc <19227912+lxmrc@users.noreply.github.com>
2020-09-28 13:32:15 +1000

    Scrabble v2

--- scrabble-score/scrabble_score.rb
+++ scrabble-score/scrabble_score.rb
@@ -1,29 +1,25 @@
 class Scrabble
-  SCORES = { 
-    'A' => 1, 'N' => 1,
-    'B' => 3, 'O' => 1,
-    'C' => 3, 'P' => 3,
-    'D' => 2, 'Q' => 10,
-    'E' => 1, 'R' => 1,
-    'F' => 4, 'S' => 1,
-    'G' => 2, 'T' => 1,
-    'H' => 4, 'U' => 1,
-    'I' => 1, 'V' => 4,
-    'J' => 8, 'W' => 4,
-    'K' => 5, 'X' => 8,
-    'L' => 1, 'Y' => 4,
-    'M' => 3, 'Z' => 10
+  SCORES = {
+      /[AEIOULNRST]/ => 1,
+      /[DG]/ => 2,
+      /[BCMP]/ => 3,
+      /[FHVWY]/ => 4,
+      /[K]/ => 5,
+      /[JX]/ => 8,
+      /[QZ]/ => 10
   }
+
+  def initialize(word)
+    @word = word.to_s.upcase
+  end
+
+  def score 
+    SCORES.sum do |letters, value|
+      @word.scan(letters).count * value
+    end 
+  end
 
   def self.score(word)
     new(word).score
-  end
-
-  def initialize(word)
-    @letters = word.to_s.upcase.scan(/[A-Z]/)
-  end
-
-  def score
-    @letters.sum(&SCORES)
   end
 end
